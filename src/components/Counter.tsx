a60e7b192009d4fa06a7f9a3bdd3b436015e49a4
Hasan Ali <hasanhaja@gmail.com>
2022-08-16 22:09:17 +0100

    Project init

---
+++ src/components/Counter.tsx
@@ -0,0 +1,17 @@
+import { ParentComponent, createSignal } from 'solid-js';
+
+const Counter: ParentComponent<{}> = () => {
+	const [count, setCount] = createSignal(0);
+	const add = () => setCount(count() + 1);
+	const subtract = () => setCount(count() - 1);
+
+	return (
+		<div class="bg-gray-200 flex justify-between">
+      <button class="bg-red-500 rounded-md px-8 py-2 text-2xl font-extrabold" onClick={subtract}>-</button>
+      <pre class="p-4 text-2xl">{count()}</pre>
+      <button class="bg-teal-500 rounded-md px-8 py-2 text-2xl font-extrabold" onClick={add}>+</button>
+		</div>
+	);
+}
+
+export default Counter;
